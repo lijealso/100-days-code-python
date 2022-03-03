a9a22d32152b2de109474b8e34ac4a04ebbab2e8
lijealso <lijealso@gmail.com>
2022-03-03 20:55:02 +0000

    day 6, reeborg3

---
+++ day006/reeborg3.py
@@ -0,0 +1,21 @@
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+while (not at_goal()) == True:
+    if front_is_clear():
+        move()
+    elif wall_in_front():
+        jump()
