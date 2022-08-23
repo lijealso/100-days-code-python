54590e16304e30d5660e6e0dadf0e3ea45dab642
lijealso <lijealso@gmail.com>
2022-08-23 22:00:28 +0100

    day 8, project

---
+++ day008/caesar-cipher.py
@@ -0,0 +1,17 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def encrypt(text, shift):
+    cipher_text = ""
+    for letter in text:
+        idx = alphabet.index(letter)
+        new_idx = idx + shift
+        new_letter = alphabet[new_idx]
+        cipher_text += new_letter
+    print(f"The encoded text is {cipher_text}")
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+if direction == 'encode':
+    encrypt(text, shift)
