94841cab0ae8ee8beba59e14d2c0c66df773bbcd
lijealso <lijealso@gmail.com>
2022-08-23 22:00:05 +0100

    day 8, exercise

---
+++ day008/start.py
@@ -0,0 +1,14 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+
+def greet(name):
+    print(f"Hello {name}!")
+    print(f"How do you do {name}?")
+    print(f"Bye {name}...")
+
+# Call the greet() function and run your code.
+
+input = input("Enter name: ")
+
+greet(input)
