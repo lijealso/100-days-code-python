825583526b1d8e2bdef73f282ebf39e0fa0ad146
lijealso <lijealso@gmail.com>
2022-08-30 22:06:01 +0100

    Update caesar-cipher.py

--- day008/caesar-cipher.py
+++ day008/caesar-cipher.py
@@ -9,9 +9,21 @@ def encrypt(text, shift):
         cipher_text += new_letter
     print(f"The encoded text is {cipher_text}")
 
+def decrypt(text, shift):
+    decipher_text = ""
+    for letter in text:
+        idx = alphabet.index(letter)
+        new_idx = idx - shift
+        new_letter = alphabet[new_idx]
+        decipher_text += new_letter
+    print(f"The decoded text is {decipher_text}")
+
+
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 if direction == 'encode':
     encrypt(text, shift)
+elif direction == 'decode':
+    decrypt(text, shift)
