c3b4bb1feb01ad933c1c40beec6d19922a416a1b
lijealso <lijealso@gmail.com>
2022-10-06 00:48:12 +0100

    day 9, exercise

---
+++ day009/exercise.py
@@ -0,0 +1,34 @@
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+#🚨 Do NOT change the code above
+
+#Write the function that will allow new countries
+#to be added to the travel_log. 👇
+
+def add_new_country(country, visits, cities):
+    travel_log.append({"country" : country, "visits" : visits, "cities" : cities})
+
+
+#🚨 Do not change the code below
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
+
+
+starting_dictionary = {
+    "a": 9,
+    "b": 8,
+}
+
+starting_dictionary["c"] = 7
+final = starting_dictionary
+print(final)
